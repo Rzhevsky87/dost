69c3d3846d3b5cb9389a4b3a8b5a9052346382a5
Rzhevsky87 <egorbarisovi4@gmail.com>
2021-02-08 15:09:58 +0500

    add FB SDK script

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -128,5 +128,14 @@
                 </div>
             </div>
         </div>
+        <script>
+            (function(d, s, id){
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) {return;}
+                js = d.createElement(s); js.id = id;
+                js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'Messenger'));
+        </script>
     </body>
 </html>
